6a10e901fd7e2383183b190795dc168ed449ae0d
Meagan <meagan@bowtie.co>
2019-03-29 15:05:14 -0600

    add polaris ui

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,47 @@
-const Index = () => (
-  <div>
-    <p>A wishlist app using React and Next.js</p>
-  </div>
-)
+import { EmptyState, Layout, Page, ResourcePicker } from '@shopify/polaris';
+
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg'
+
+class Index extends React.Component {
+
+  state = {open: false};
+
+    render() {
+      return (
+        <Page
+          primaryAction={{
+            content: 'Select products',
+            onAction: () => this.setState({ open: true })
+          }}
+        >
+          <ResourcePicker
+            resourceType="Product"
+            showVariants={false}
+            open={this.state.open}
+            onSelection={(resources) => this.handleSelection(resources)}
+            onCancel ={() => this.setState({ open: false })}
+          />
+          <Layout>
+            <EmptyState
+              heading="Create a wishlist to save great products"
+              action={{
+                content: 'Select products',
+                onAction: () => this.setState({ open: true })
+              }}
+              image={img}
+            >
+              <p>Select products to add to your wishlist.</p>
+            </EmptyState>
+          </Layout>
+        </Page>
+    );
+  }
+
+  handleSelection = (resources) => {
+    const idsFromResources = resources.selection.map((product) => product.id)
+    this.setState({ open: false })
+    console.log(resources)
+  }
+}
 
 export default Index;
